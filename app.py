99ed5aa2a75dfbc0c90b78ef7d8b166fc881147d
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-21 12:14:02 -0500

    Finished setting up ORM mapping of main table (need to create actual table)

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Date
+from sqlalchemy import String
+from datetime import date
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:<YOUR MYSQL PASSWORD>@localhost/<YOUR DATABASE>'
@@ -22,5 +23,20 @@ class Member(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     email: Mapped[str] = mapped_column(String(360), nullable=False, unique=True)
-    DOB: Mapped[Date]
+    DOB: Mapped[date]
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+    
+class Loan(Base):
+    __tablename_ = "loans"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author: Mapped[str] = mapped_column(String(255), nullable=False)
+    genre: Mapped[str] = mapped_column(String(255), nullable=False)
+    desc: Mapped[str] = mapped_column(String(255), nullable=False)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    
+# Create the table
+with app.app_context():
+    db.create_all()
+    
+app.run()
