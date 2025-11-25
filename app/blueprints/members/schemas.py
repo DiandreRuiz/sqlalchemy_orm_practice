aaa97cecd50cf68218f9fb8ca6bff38807619655
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-25 12:48:30 -0500

    Light formatting

--- app/blueprints/members/schemas.py
+++ app/blueprints/members/schemas.py
@@ -4,5 +4,6 @@ from app.models import Member
 class MemberSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Member
+        
 member_schema = MemberSchema()
 members_schema = MemberSchema(many=True)
